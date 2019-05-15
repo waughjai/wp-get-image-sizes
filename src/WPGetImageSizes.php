05cb8a93c654058f2052b927a1ea1827a85633d3
Jaimeson Waugh <jaimeson@4cesi.com>
2019-05-15 13:05:04 -0700

    Clean up code

--- src/WPGetImageSizes.php
+++ src/WPGetImageSizes.php
@@ -1,47 +1,44 @@
 <?php
 
 declare( strict_types = 1 );
-namespace WaughJ\WPGetImageSizes
+namespace WaughJ\WPGetImageSizes;
+
+const DEFAULT_SIZES = [ 'thumbnail', 'medium', 'medium_large', 'large' ];
+
+function WPGetImageSizes() : array
 {
-	use WaughJ\WPGetImageSizes\WPImageSize;
-
-	const DEFAULT_SIZES = [ 'thumbnail', 'medium', 'medium_large', 'large' ];
-
-	function WPGetImageSizes() : array
+	global $_wp_additional_image_sizes;
+	$image_sizes = [];
+	$default_image_sizes = get_intermediate_image_sizes();
+    foreach ( $default_image_sizes as $size )
 	{
-		global $_wp_additional_image_sizes;
-		$image_sizes = [];
-		$default_image_sizes = get_intermediate_image_sizes();
-	    foreach ( $default_image_sizes as $size )
+		if ( in_array( $size, DEFAULT_SIZES ) )
 		{
-			if ( in_array( $size, DEFAULT_SIZES ) )
-			{
-				$image_sizes[] = new WPImageSize( ( string )( $size ), intval( get_option( "{$size}_size_w" ) ), intval( get_option( "{$size}_size_h" ) ) );
-			}
-			else if ( isset( $_wp_additional_image_sizes[ $size ] ) )
-			{
-				$image_sizes[] = new WPImageSize( ( string )( $size ), $_wp_additional_image_sizes[ $size ][ 'width' ], $_wp_additional_image_sizes[ $size ][ 'height' ] );
-			}
-	    }
-	    return SortImageSizeList( $image_sizes );
-	}
+			$image_sizes[] = new WPImageSize( ( string )( $size ), intval( get_option( "{$size}_size_w" ) ), intval( get_option( "{$size}_size_h" ) ) );
+		}
+		else if ( isset( $_wp_additional_image_sizes[ $size ] ) )
+		{
+			$image_sizes[] = new WPImageSize( ( string )( $size ), $_wp_additional_image_sizes[ $size ][ 'width' ], $_wp_additional_image_sizes[ $size ][ 'height' ] );
+		}
+    }
+    return SortImageSizeList( $image_sizes );
+}
 
-	function SortImageSizeList( $image_sizes ) : array
-	{
-		usort
-		(
-			$image_sizes,
-			function( WPImageSize $a, WPImageSize $b ) : int
+function SortImageSizeList( $image_sizes ) : array
+{
+	usort
+	(
+		$image_sizes,
+		function( WPImageSize $a, WPImageSize $b ) : int
+		{
+			$aw = $a->getWidth();
+			$bw = $b->getWidth();
+			if ( $aw === $bw )
 			{
-				$aw = $a->getWidth();
-				$bw = $b->getWidth();
-				if ( $aw === $bw )
-				{
-					return 0;
-				}
-				return ( $aw < $bw ) ? -1 : 1;
+				return 0;
 			}
-		);
-		return $image_sizes;
-	}
+			return ( $aw < $bw ) ? -1 : 1;
+		}
+	);
+	return $image_sizes;
 }
--- src/WPImageSize.php
+++ src/WPImageSize.php
@@ -1,34 +1,33 @@
 <?php
 
 declare( strict_types = 1 );
-namespace WaughJ\WPGetImageSizes
+namespace WaughJ\WPGetImageSizes;
+
+class WPImageSize
 {
-	class WPImageSize
+	public function __construct( string $slug, int $width, int $height )
 	{
-		public function __construct( string $slug, int $width, int $height )
-		{
-			$this->slug = $slug;
-			$this->width = $width;
-			$this->height = $height;
-		}
-
-		public function getSlug() : string
-		{
-			return $this->slug;
-		}
-
-		public function getWidth() : int
-		{
-			return $this->width;
-		}
-
-		public function getHeight() : int
-		{
-			return $this->height;
-		}
-
-		private $slug;
-		private $width;
-		private $height;
+		$this->slug = $slug;
+		$this->width = $width;
+		$this->height = $height;
 	}
+
+	public function getSlug() : string
+	{
+		return $this->slug;
+	}
+
+	public function getWidth() : int
+	{
+		return $this->width;
+	}
+
+	public function getHeight() : int
+	{
+		return $this->height;
+	}
+
+	private $slug;
+	private $width;
+	private $height;
 }
